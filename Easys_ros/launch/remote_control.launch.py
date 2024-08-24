27f87d208df678af80cb118e3567c85a9a66bc07
tamago117 <taikibannsei2020@gmail.com>
2024-08-24 04:53:48 +0000

    add rviz2 config

--- Easys_ros/launch/remote_control.launch.py
+++ Easys_ros/launch/remote_control.launch.py
@@ -20,12 +20,6 @@ def generate_launch_description():
             )
     launch_description_list.append(joy_node)
 
-    rqt_image_view = Node(
-            package='rqt_image_view',
-            executable='rqt_image_view',
-            )
-    launch_description_list.append(rqt_image_view)
-
     uncompressed_image = Node(
             package='image_transport',
             executable='republish',
@@ -50,5 +44,13 @@ def generate_launch_description():
                 )
     launch_description_list.append(yolov8_launch_cmd)
 
+    # rviz2
+    rviz2 = Node(
+                package='rviz2',
+                executable='rviz2',
+                arguments=['-d', os.path.join(get_package_share_directory('Easys_ros'), 'config', 'easys.rviz')],
+                output='screen'
+    )
+    launch_description_list.append(rviz2)
 
     return LaunchDescription(launch_description_list)
